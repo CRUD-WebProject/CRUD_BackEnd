6003c5435869601458eb2cbe0d130f821f350aff
jiwoo0629 <wldn990629@gmail.com>
2023-02-19 13:45:28 +0900

    Post, User, Comment 각 entity 생성

---
+++ src/main/java/com/example/crud_backend/domain/Posts.java
@@ -0,0 +1,36 @@
+package com.example.crud_backend.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Getter
+@Entity
+public class Posts {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long postID; //PK -> auto increment
+    @Column(length=20, nullable=false)
+    private String id;
+    @Column(length=100, nullable=false)
+    private String title;
+    @Column(columnDefinition="TEXT", nullable=false)
+    private String content;
+    @Column(length=50, nullable=false)
+    private String category;
+    private LocalDateTime up_time;
+    private int likes;
+    private int unlikes;
+
+    @Builder
+    public Posts(String id, String title, String content, String category) {
+        this.id=id;
+        this.title=title;
+        this.content=content;
+        this.category=category;
+        this.up_time=LocalDateTime.now();
+        this.likes=0;
+        this.unlikes=0;
+    }
+
+}
---
+++ src/main/java/com/example/crud_backend/domain/Users.java
@@ -0,0 +1,33 @@
+package com.example.crud_backend.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@NoArgsConstructor
+@Getter
+@Entity
+public class Users {
+    @Id
+    private String id;
+    @Column(length=20, nullable=false)
+    private String name;
+    @Column(length=20, nullable=false)
+    private String pw;
+    @Column(length=1, nullable=false)
+    private String sex;
+    @Column(nullable=false)
+    private Long age;
+    private String phone;
+    private String email;
+
+    @Builder
+    public Users (String id, String name, String pw, String sex, Long age, String phone, String email) {
+        this.id=id;
+        this.name=name;
+        this.pw=pw;
+        this.sex=sex;
+        this.age=age;
+        this.phone=phone;
+        this.email=email;
+    }
+}
